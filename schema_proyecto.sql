dc97d801d453e88cc27c15beea7791bfcc90b4af
demonvaa <ernestovaamonde@yahoo.es>
2026-04-29 17:59:21 +0200

    Actualizacion a 200 y orden de tablas

--- schema_proyecto.sql
+++ schema_proyecto.sql
@@ -1,10 +1,28 @@
 CREATE SCHEMA Spotify_proyect;
 USE Spotify_proyect;
 
+DROP TABLE IF EXISTS country;
+CREATE TABLE IF NOT EXISTS Spotify_proyect.country (
+country_id INT PRIMARY KEY AUTO_INCREMENT,
+country VARCHAR(30)
+);
+
+DROP TABLE IF EXISTS artist;
+CREATE TABLE IF NOT EXISTS Spotify_proyect.artist (
+artist_id INT PRIMARY KEY AUTO_INCREMENT,
+artist VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS genre;
+CREATE TABLE IF NOT EXISTS Spotify_proyect.genre (
+genre_id INT PRIMARY KEY AUTO_INCREMENT,
+genre VARCHAR (30)
+);
+
 DROP TABLE IF EXISTS top50_country;
 CREATE TABLE IF NOT EXISTS Spotify_proyect.top50_country (
 id INT PRIMARY KEY AUTO_INCREMENT,
-title VARCHAR(100),
+title VARCHAR(200),
 artist_id INT NOT NULL,
 FOREIGN KEY (artist_id) REFERENCES artist (artist_id) ON DELETE CASCADE, 
 genre_id INT NOT NULL,
@@ -24,23 +42,5 @@ spch FLOAT,
 pop INT,
 country_id INT NOT NULL,
 FOREIGN KEY (country_id) REFERENCES country (country_id) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS country;
-CREATE TABLE IF NOT EXISTS Spotify_proyect.country (
-country_id INT PRIMARY KEY AUTO_INCREMENT,
-country VARCHAR(30)
-);
-
-DROP TABLE IF EXISTS artist;
-CREATE TABLE IF NOT EXISTS Spotify_proyect.artist (
-artist_id INT PRIMARY KEY AUTO_INCREMENT,
-artist VARCHAR(100)
-);
-
-DROP TABLE IF EXISTS genre;
-CREATE TABLE IF NOT EXISTS Spotify_proyect.genre (
-genre_id INT PRIMARY KEY AUTO_INCREMENT,
-genre VARCHAR (30)
 );
 
