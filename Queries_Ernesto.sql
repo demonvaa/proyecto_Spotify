123855cc572835c4745b1cedf9de6c895e941169
demonvaa <ernestovaamonde@yahoo.es>
2026-04-30 22:16:07 +0200

    Añadidos Query_Ernesto.sql y notebook donde se ve como quedarian esas graficas

--- Queries_Ernesto.sql
+++ Queries_Ernesto.sql
@@ -1,31 +1,26 @@
 -- (Cuales son los 10 paises donde suenan mas canciones del ToP 50 )
 SELECT 
-    title,
-    COUNT(DISTINCT country_id) AS num_paises
-FROM top50_country
-GROUP BY title
-HAVING COUNT(DISTINCT country_id) > 1
+    t.title,
+    a.artist, 
+    COUNT(DISTINCT t.country_id) AS num_paises
+FROM top50_country t
+JOIN artist a ON t.artist_id = a.artist_id
+GROUP BY t.title, a.artist
+HAVING COUNT(DISTINCT t.country_id) > 1
 ORDER BY num_paises DESC
 LIMIT 20;
 
--- Top 1 género por país (el dominante)
-SELECT
-pais,
-genero AS genero_dominante
-FROM (
-    SELECT 
-        c.country AS pais,
-        g.genre AS genero,
-        ROW_NUMBER() OVER(
-            PARTITION BY c.country
-            ORDER BY COUNT(*) DESC
-        ) AS rn
-	FROM top50_country t
-	JOIN country c ON t.country_id = c.country_id
-	JOIN genre g ON t.genre_id = g.genre_id
-	GROUP BY c.country, g.genre
-) AS subconsulta
-WHERE rn =1
-ORDER BY pais, genero DESC
+-- Artista mas bailable y su genero
+SELECT 
+	AVG(t.dnce) AS dnce_media,
+    a.artist,
+    g.genre
+FROM top50_country t
+JOIN artist a ON t.artist_id = a.artist_id
+JOIN genre g ON t.genre_id = g.genre_id
+GROUP BY a.artist, g.genre
+ORDER BY dnce_media DESC;
+
+
 
 
