11e1e9010c90db68e53544c586fe6a3b31a74b4f
demonvaa <ernestovaamonde@yahoo.es>
2026-05-01 10:31:08 +0200

    Archivo Query y NOTEBOOK MODIFICADOS 10:31

--- Queries_Ernesto.sql
+++ Queries_Ernesto.sql
@@ -1,8 +1,9 @@
--- (Cuales son los 10 paises donde suenan mas canciones del ToP 50 )
+-- (Cuales son las canciones mas cantadas del top 50 y su artista )
+USE spotify_proyect;
 SELECT 
-    t.title,
-    a.artist, 
-    COUNT(DISTINCT t.country_id) AS num_paises
+    t.title as Titulo_mas_famoso,
+    COUNT(DISTINCT t.country_id) AS num_paises,
+	a.artist as Artista
 FROM top50_country t
 JOIN artist a ON t.artist_id = a.artist_id
 GROUP BY t.title, a.artist
@@ -20,7 +21,5 @@ JOIN artist a ON t.artist_id = a.artist_id
 JOIN genre g ON g.genre_id = t.genre_id
 GROUP BY a.artist, g.genre
 ORDER BY dnce_media DESC;
-
-
 
 
